c95687f0facd2231aa7979964e408ab1cced2e10
belyabl9 <belyabl9@gmail.com>
2014-05-09 01:43:25 +0300

    add FindServlet

---
+++ src/ua/net/forum/servlets/FindServlet.java
@@ -0,0 +1,52 @@
+package ua.net.forum.servlets;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import model.Message;
+import service.IMessageService;
+import service.ServiceFactory;
+
+/**
+ * Servlet implementation class FindServlet
+ */
+@WebServlet("/FindServlet")
+public class FindServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FindServlet() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String pattern = request.getParameter("find");
+		IMessageService msgService = ServiceFactory.DEFAULT.getMessageService();
+		Collection<Message> messages = msgService.getAllEntites();
+		for (Message msg : messages) {
+			String content = msg.getContent();
+			if (content.lastIndexOf(pattern) != -1) {
+				System.out.println(content);
+			}
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
